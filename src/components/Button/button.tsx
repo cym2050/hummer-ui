1cea19d52799406c3099da985abf4aa8c500c187
cym2050 <1390062269@qq.com>
2020-09-10 14:49:00 +0800

    取得butto所有属性

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -22,16 +22,22 @@ interface BaseButtonProps {
     href ?: string;
 }
 
-const Button: React.FC<BaseButtonProps> = (props) => {
+type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+
+const Button: React.FC<ButtonProps> = (props) => {
     const {
         disabled,
+        className,
         size,
         btnType,
         children,
-        href
+        href,
+        ...restProps
     } = props;
 
-    const classes = classNames('btn', {
+    const classes = classNames('btn', className, {
         [`btn-${btnType}`]: btnType,
         [`btn-${size}`]: size,
         'disabled': (btnType === ButtonType.Link) && disabled
@@ -39,7 +45,7 @@ const Button: React.FC<BaseButtonProps> = (props) => {
 
     if ( btnType === ButtonType.Link && href ) {
         return (
-            <a className={classes} href={href}>
+            <a className={classes} href={href} {...restProps}>
                 { children }
             </a>
         )
@@ -48,6 +54,7 @@ const Button: React.FC<BaseButtonProps> = (props) => {
             <button
                 className={classes}
                 disabled={disabled}
+                {...restProps}
             >
                 { children }
             </button>
