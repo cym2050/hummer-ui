c3f68a0b17c50c9efd862ab74e57975feee34ac5
cym2050 <1390062269@qq.com>
2020-09-10 22:01:55 +0800

    useContext包含index和回调函数

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu onSelect={(e) => alert(e)}>
+            <MenuItem index={0}>
+              link1
+            </MenuItem>
+            <MenuItem index={1}>
+              link2
+            </MenuItem>
+            <MenuItem index={2}>
+              link3
+            </MenuItem>
+        </Menu>
+
 
         <Button>MyButton</Button>
         <Button size={ButtonSize.Small} btnType={ButtonType.Danger}>MyButton</Button>
         <Button size={ButtonSize.Large}>MyButton</Button>
         <Button disabled>disabled</Button>
-        
         <Button btnType={ButtonType.Link} href='www.baidu.com' disabled>disabled</Button>
 
       </header>
