7fbfe03631b4d46d3914dd2efb27d30aa0f7b8f7
cym2050 <1390062269@qq.com>
2020-09-10 16:13:45 +0800

    为Button添加单元测试

---
+++ src/components/Button/button.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import Button, { ButtonProps, ButtonSize, ButtonType } from './button'
+
+const defaultProps = {
+  onClick: jest.fn()
+}
+
+
+
+describe('test Button component', () => {
+  it('should render the correct default button', () => {
+    const wrapper = render(<Button {...defaultProps}>Nice</Button>)
+    const element = wrapper.getByText('Nice')
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('BUTTON') //tagName 都是大写
+    expect(element).toHaveClass('btn btn-default')
+    fireEvent.click(element)
+    expect(defaultProps.onClick).toBeCalled()
+  })
+
+})
