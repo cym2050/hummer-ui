f988228d9145a59be01220190d17c0255440a177
cym2050 <1390062269@qq.com>
2020-09-26 15:08:54 +0800

    添加 Home Doc 组件

---
+++ src/views/Doc.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import Button from '../components/Button/button'
+import Menu from '../components/Menu/menu'
+import MenuItem from '../components/Menu/menuItem'
+import SubMenu from '../components/Menu/subMenu'
+import Icon from '../components/Icon/icon'
+import Transition from '../components/Transition/transition'
+
+function Doc() {
+  const [ show, setShow ] = useState(false)
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <FontAwesomeIcon icon={faCoffee} /> 变量形式引用 */}
+        {/* <Icon icon={faCoffee} theme='danger' size='10x' /> */}
+        {/* <FontAwesomeIcon icon='arrow-down' /> */}
+        {/* <Icon icon='arrow-down' theme='danger' size='10x' /> */}
+        <Menu onSelect={(index) => { alert(index)}}  defaultOpenSubMenus={['3']}>
+            <MenuItem disabled>
+              link1
+            </MenuItem>
+            <MenuItem>
+              link2
+            </MenuItem>
+            <MenuItem>
+              link3
+            </MenuItem>
+            <SubMenu title='dropdown'>
+              <MenuItem>
+                sdfsd 1
+              </MenuItem>
+              <MenuItem>
+                sdfsd 2
+              </MenuItem>
+            </SubMenu>
+        </Menu>
+        <Button size='lg' onClick={ () => { setShow(!show) } }> Toggle </Button>
+        <Transition in={show} timeout={200} animation='zoom-in-top' wrapper>
+          <div>
+            <p>nihaoasdfasdfas</p>
+            <p>nihaoasdfasdfadsf</p>
+            <p>nihaofasdfadsfadsf</p>
+            <p>nihaoasdfasdfadsf</p>
+            <p>nihaofasdfadsfadsf</p>
+            <p>nihaoasdfasdfadsf</p>
+            <p>nihaofasdfadsfadsf</p>
+          </div>
+          <Button btnType='primary' size='lg'>asdfadfa</Button>
+        </Transition>
+      </header>
+    </div>
+  )
+}
+
+export default Doc
